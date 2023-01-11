14b0a3f981264c9a3ef855135b6646b4e58383d6
Bas-sheikh <basilelshakhe@gmail.com>
2023-01-12 00:54:48 +0200

    handled post in save page and handled add to save list request function. relates #20

--- client/src/features/auth/authService.ts
+++ client/src/features/auth/authService.ts
@@ -3,7 +3,6 @@ import axios from '../../api/axios';
 const login = async () => {
   try {
     const response = await axios.get('auth/user', { withCredentials: true });
-    console.log(`data from login middleware for redux ${response}`)
     if (response.status === 200) return response.data;
     else throw response;
   } catch (error) {
